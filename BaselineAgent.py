54de1ce9ae28217de9cc1c7916969893ae8ab15c
elitack <lzg960702@163.com>
2018-05-12 17:21:40 +0800

    change the init

--- BaselineAgent.py
+++ BaselineAgent.py
@@ -5,9 +5,21 @@ from model_simple import RRL
 from Data import DM
 from Agent import Agent
 
-class BaselineAgent(Agent):
+class BaselineAgent(object):
     def __init__(self, config):
-        super().__init__(config)
+        self.DM = DM()
+        self.batch_size = config['batch_size']
+        self.config = config
+
+        self.DM.input_range(config['start_date'], config['end_date'], config['time_span'], config['stocks'])
+
+        self.p_train = config['p_train']
+        self.p_vali = config['p_vali']
+        self.t_num = self.feature.shape[0]
+        self.t_num_train = int(self.t_num * self.p_train)
+        self.t_num_vali = int(self.t_num * self.p_vali)
+        self.t_num_test = self.t_num - self.t_num_train - self.t_num_vali
+        self.s_num = self.rise_percent.shape[1]    
         self.feature, self.rise_percent = self.DM.gen_data_baseline(self.config["fea_dim"])
 
     def best_stock(self):
