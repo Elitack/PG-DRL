3d977e5bf9e3cdb1d18d7249cc71f7ab4ed14b25
elitack <lzg960702@163.com>
2018-05-28 19:50:19 +0800

    ou process and replay buffer

---
+++ DDPG/tool.py
@@ -0,0 +1,55 @@
+import random
+import numpy as np
+from collections import deque
+
+class OU_Process(object):
+    def __init__(self, action_dim, theta=0.15, mu=0, sigma=0.2):
+        self.action_dim = action_dim
+        self.theta = theta
+        self.mu = mu
+        self.sigma = sigma
+        self.current_x = None
+
+        self.init_process()
+
+    def init_process(self):
+        self.current_x = np.ones(self.action_dim) * self.mu
+
+    def update_process(self):
+        dx = self.theta * (self.mu - self.current_x) + self.sigma * np.random.randn(self.action_dim)
+        self.current_x = self.current_x + dx
+
+    def return_noise(self):
+        self.update_process()
+        return self.current_x
+
+class Replay_Buffer(object):
+    def __init__(self, buffer_size=10e6, batch_size=1):
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
+        self.memory = deque(maxlen=buffer_size)
+
+    def __call__(self):
+        return self.memory
+
+    def store_transition(self, transition):
+        self.memory.append(transition)
+
+    def store_transitions(self, transitions):
+        self.memory.extend(transitions)
+
+    def get_batch(self, batch_size=None):
+        b_s = batch_size or self.batch_size
+        cur_men_size = len(self.memory)
+        if cur_men_size < b_s:
+            return random.sample(list(self.memory), cur_men_size)
+        else:
+            return random.sample(list(self.memory), b_s)
+
+    def memory_state(self):
+        return {"buffer_size": self.buffer_size,
+                "current_size": len(self.memory),
+                "full": len(self.memory)==self.buffer_size}
+
+    def empty_transition(self):
+        self.memory.clear()
