f5595809c359d8600f4712453b5a46640d1028cc
elitack <lzg960702@163.com>
2018-06-28 09:48:20 +0800

    change to ac

---
+++ DDPG/RLAgent.py
@@ -0,0 +1,151 @@
+import tensorflow as tf
+import numpy as np
+
+from model_Attention import RRL
+from Data import DM
+
+class RLAgent(object):
+    def __init__(self, config):
+        self.DM = DM()
+        self.batch_feature = config['batch_feature']
+        self.batch_f = config['batch_f']
+        self.batch_prev = self.batch_feature - self.batch_f
+        self.config = config
+
+        self.DM.input_range(config['start_date'], config['split_date'], config['end_date'], config['time_span'], config['stocks'])
+        data = self.DM.gen_data_RL(self.config["fea_dim"], pre=self.batch_prev)   
+
+        self.valid = True
+
+        if data is not None:
+            self.train_fea, self.train_rp, self.train_p = data[0]
+            self.test_fea, self.test_rp, self.test_p = data[1]
+
+            self.t_num_train = self.train_fea.shape[0] - self.batch_prev
+            self.t_num_test = self.test_fea.shape[0] - self.batch_prev
+            self.s_num = self.train_rp.shape[1]       
+
+            self.RL = RRL(config)   
+
+        else:
+            self.valid = False
+        
+
+    def RL_train(self, epochs=100):
+        train_fea = self.train_fea
+        train_rp = self.train_rp
+        train_p = self.train_p  
+
+        test_fea = self.test_fea
+        test_rp = self.test_rp
+        test_p = self.test_p[self.batch_prev:]
+
+        self.PVM = np.ones((self.t_num_train, self.s_num)) / self.s_num
+
+        train_idx = np.arange(self.t_num_train-self.batch_f)
+        np.random.shuffle(train_idx)
+        init_op = tf.global_variables_initializer()
+
+        result_list = []
+
+        config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+        config.gpu_options.allow_growth=True
+        with tf.Session(config=config) as sess:
+            sess.run(init_op)
+            self.RL.assign_lr(sess, self.config['lr'])
+            for epo in range(epochs):
+                R = 0
+                print("epoch: {}".format(epo))
+                for idx in train_idx:
+                    epo_fea = train_fea[idx:idx+self.batch_feature]
+                    epo_rp = train_rp[idx:idx+self.batch_f]
+                    if idx == 0:
+                        prev = np.ones(self.train_rp.shape[1]) / self.train_rp.shape[1]
+                    else:
+                        prev = self.PVM[idx-1]
+                    f, r = self.RL.run_epoch(sess, epo_fea, epo_rp, self.RL.adam_op, prev)
+                    R += np.sum(r)
+                    self.PVM[idx:idx+self.batch_f] = f
+                test_f, test_r = self.RL_test(sess, test_fea, test_rp)
+                result = self.evaluation(test_p, test_f, self.t_num_test*self.config['time_span']/240)
+                result_list.append(result)
+
+        result_list = np.asarray(result_list)
+        arg_max = result_list[:, 0].argmax()
+        result = result_list[arg_max]
+
+        return arg_max, result
+
+
+    def RL_test(self, sess, feature, rise_percent):
+        prev = np.ones(rise_percent.shape[1]) / rise_percent.shape[1]
+        time_step = feature.shape[0]
+        batch_num = (time_step - self.batch_prev) // self.batch_f
+        padding_num = (time_step - self.batch_prev) % self.batch_f
+        total_reward = []
+        total_f = []
+        if padding_num == 0:
+            for iter in range(batch_num):
+                test_fea = feature[iter*self.batch_f:(iter+1)*self.batch_f+self.batch_prev]
+                test_rp = rise_percent[iter*self.batch_f:(iter+1)*self.batch_f]
+                
+                test_f, test_r = self.RL.run_test_epoch(sess, test_fea, test_rp, prev)
+
+                prev = test_f[-1]
+                total_reward.extend(list(test_r))
+                total_f.append(test_f)
+            total_f = np.array(total_f)
+            total_f = total_f.reshape(-1, total_f.shape[2])
+        else:
+
+            feature = np.concatenate((feature, np.zeros((padding_num, feature.shape[1], feature.shape[2], feature.shape[3]))), axis=0)
+            rise_percent = np.concatenate((rise_percent, np.zeros((padding_num, rise_percent.shape[1]))), axis=0)
+
+            for iter in range(batch_num+1):
+                test_fea = feature[iter*self.batch_f:(iter+1)*self.batch_f+self.batch_prev]
+                test_rp = rise_percent[iter*self.batch_f+self.batch_prev:(iter+1)*self.batch_f+self.batch_prev]
+                test_f, test_r = self.RL.run_test_epoch(sess, test_fea, test_rp, prev)
+
+                prev = test_f[-1]
+                total_reward.extend(list(test_r))
+                total_f.append(test_f)
+            total_reward = total_reward[:-padding_num]
+            total_f = np.array(total_f)
+            total_f = total_f.reshape(-1, total_f.shape[2])
+            total_f = total_f[:-padding_num]
+        return total_f, total_reward
+
+    def evaluation(self, price, portfolio, days):
+        money_sequence = []
+        money = 100
+        own = np.zeros(price.shape[1])
+        for time_step in range(price.shape[0]):
+            money = money + np.sum(own * price[time_step, :])
+            money_sequence.append(money)
+            own_new = money * portfolio[time_step, :] / price[time_step, :]
+            fee = self.config["cost"] * np.sum(np.abs(own_new - own) * price[time_step, :])
+            money = money - fee
+            own = money * portfolio[time_step, :] / price[time_step, :]
+            money = 0
+        money_sequence.append(money + np.sum(own * price[time_step, :]))
+        print(self.metrics(np.array(money_sequence), days))
+        return self.metrics(np.array(money_sequence), days)
+
+    def metrics(self, seq, days):
+        length = len(seq)
+        ret = seq[1:] - seq[:-1]
+        ret_rate = (seq[1:] - seq[:-1]) / seq[:-1]
+
+        ar = seq[-1] / seq[0]
+        ar = np.power(ar, 240 / days) - 1
+        sr = ret_rate.mean() / ret_rate.std()
+        vol = ret.std()
+        ir = ret.mean() / ret.std()
+        md = -ret_rate.min()
+
+        return [round(ar, 4), round(sr, 4), round(vol, 4), round(ir, 4), round(md, 4)]
+
+
+
+
+
---
+++ DDPG/model_Attention.py
@@ -0,0 +1,170 @@
+import tensorflow as tf
+import numpy as np
+
+class RRL(object):
+    
+    ''' Model Parameters '''
+    def _weight_variable(self, shape, name=""):
+        return tf.get_variable(name, shape, tf.float32)
+
+    def _bias_variable(self, shape, name=""):
+        return tf.get_variable(name, shape, tf.float32)
+
+    ''' Stock Scoring Module '''
+    def _cnn_net(self, state_vec, name=""):
+        '''
+        A linear model for generating score of each stock, by linearly combining their feature vectors.
+        :param state_vec: stock_num * feature dimension
+        :param name: optional name
+        :return: stock score with size [stock_num * 1]
+        '''
+        shape = state_vec.get_shape().as_list()
+        conv1 = tf.nn.relu(\
+            tf.nn.conv2d(state_vec, self._weight_variable([1, 3, 3, 2], 'conv1_w'), [1, 1, 1, 1], "VALID")\
+            + self._bias_variable([2], 'conv1_b'))
+        conv2 = tf.squeeze(\
+            tf.nn.conv2d(conv1, self._weight_variable([1, self.config["fea_dim"]-2, 2, 1], 'conv2_w'), [1, 1, 1, 1], "VALID"))
+        return conv2
+
+    def _score2f(self, score, axis=-1):
+ 
+        return tf.nn.softmax(score, dim=axis)
+
+    def __init__(self, config):
+        self.config = config
+
+        batch_feature = config['batch_feature']
+        batch_f = config['batch_f']
+        batch_prev = batch_feature - batch_f
+
+        self.state_vec = state_vec = \
+                tf.placeholder(dtype=tf.float32, shape=[batch_feature, config['stock_num'], config["fea_dim"], 3], name="state_vec") 
+        self.Fp = Fp = \
+                tf.placeholder(dtype=tf.float32, shape=[config["stock_num"], 1], name="Portfolio_previous")
+        self.rise_percent = rise_percent = \
+                tf.placeholder(dtype=tf.float32, shape=[batch_f, config["stock_num"]], name="rise_percent")
+        
+        self.lr = lr = \
+                tf.Variable(0.0, trainable=False, name="Learning_Rate")
+
+
+
+        cat_w = self._weight_variable([config["stock_num"], config["stock_num"] * 2], "cat2policy_w")
+        cat_b = self._bias_variable([config["stock_num"], 1], "cat2policy_b")   
+
+        p2o_w = self._weight_variable([config["stock_num"], 1], "p2o_w")
+        p2o_b = self._bias_variable([batch_feature, 1], "p2o_b")     
+
+        attention_layer = self._weight_variable([batch_prev+1, 1], "attention")
+
+        score = self._cnn_net(state_vec, 'CNN')
+        score = tf.expand_dims(self._score2f(score), 0)
+        lstm_cell = tf.contrib.rnn.BasicLSTMCell(num_units=config["stock_num"], forget_bias=1.0, state_is_tuple=True)
+        init_state = lstm_cell.zero_state(1, dtype=tf.float32)
+        unprocessed_F = []
+        with tf.variable_scope('RNN'):
+            for timestep in range(batch_feature):
+                if timestep > 0:
+                    tf.get_variable_scope().reuse_variables()
+                (cell_output, state) = lstm_cell(score[:, timestep, :], init_state)
+                unprocessed_F.append(tf.squeeze(self._score2f(cell_output)))
+        unprocessed_F = tf.stack(unprocessed_F)
+        latent_v = tf.sigmoid(tf.matmul(unprocessed_F, p2o_w)+p2o_b)
+
+        processed_F = []
+        for item in range(batch_f):
+            batch_v = tf.slice(latent_v, [item, 0], [batch_prev+1, -1])
+            batch_p = tf.slice(unprocessed_F, [item, 0], [batch_prev+1, -1])
+            ratio = tf.nn.softmax(tf.multiply(batch_v, attention_layer), dim=0)
+
+            ele_f = tf.matmul(tf.transpose(batch_p), ratio)
+            processed_F.append(ele_f)
+
+        final_F = []
+        list_reward = []
+        for item in range(batch_f):
+            rise_percent_t = tf.squeeze(tf.slice(rise_percent, [item, 0], [1, -1]))
+            if item == 0:
+                cat_layer = tf.concat([processed_F[item], Fp], axis=0)
+                score_cat = tf.matmul(cat_w, cat_layer) + cat_b
+                F = self._score2f(score_cat, 0)
+
+                # RL reward
+                Rt = tf.reduce_sum(tf.multiply(F, tf.transpose(rise_percent))) - \
+                        config["cost"] * tf.reduce_sum(tf.abs(F-Fp)) # config["cost"] = 0.003, turnover cost
+
+                final_F.append(tf.squeeze(F))
+                list_reward.append(Rt)
+            else:
+                cat_layer = tf.concat([processed_F[item], F], axis=0)
+                score_cat = tf.matmul(cat_w, cat_layer) + cat_b
+                F = self._score2f(score_cat, 0)
+
+                # RL reward
+                Rt = tf.reduce_sum(tf.multiply(F, tf.transpose(rise_percent))) - \
+                        config["cost"] * tf.reduce_sum(tf.abs(F-Fp)) # config["cost"] = 0.003, turnover cost
+
+                final_F.append(tf.squeeze(F))
+                list_reward.append(Rt)
+        
+
+        self.final_F = tf.stack(final_F)
+        self.reward = tf.stack(list_reward)
+        # RL optimization part
+        adam_optimizer = tf.train.AdamOptimizer(learning_rate = lr)
+        self.adam_op = adam_optimizer.minimize(-tf.reduce_sum(self.reward))
+
+        # Subgraph for learning rate adaptation
+        self._new_lr = tf.placeholder(tf.float32, shape=[], name="new_lr")
+        self._lr_update = tf.assign(self.lr, self._new_lr)
+
+    def assign_lr(self, session, lr_value):
+        session.run(self._lr_update, feed_dict={self._new_lr: lr_value})
+
+
+    def run_step(self, session, state_vec, rise_percent, Fp, op):
+        ''' 
+        Stepwise Trading
+        :param session: tensorflow session
+        :param state_vec: state vector on stocks in trading days
+        :param rise_percent: rise_percent of all stocks in trading days
+        :param op: optimization option
+        :return: portfolios, sharpe ratio, variable of policy net
+        '''
+        feed_dict = {}
+        feed_dict[self.Fp] = Fp
+        feed_dict[self.state_vec] = state_vec
+        feed_dict[self.rise_percent] = rise_percent
+
+        # Do optimization
+        fetches = [self.final_F, self.reward, op]
+        portfolio, reward, _ = session.run(fetches, feed_dict)
+        return portfolio, reward
+    
+    def run_epoch(self, session, state_vec, rise_percent, op, Prev):
+        '''
+        Epoch-wise Runing
+        :param session: tensorflow session
+        :param state_vec: state vec of all stocks in trading days
+        :param rise_percent: rise_percent of all stocks in trading days
+        :param op: optimization option
+        :return: Sharpe Ratio, Portfolios
+        '''
+        times, stock_num, fea_dim, channel = state_vec.shape
+        total_reward = 0
+        portfolios, reward = \
+                self.run_step(session, state_vec, rise_percent, Prev.reshape(-1, 1), op)
+        return portfolios, reward
+
+    def run_test_epoch(self, session, state_vec, rise_percent, Prev):
+        '''
+        For test
+        '''
+        times, stock_num, fea_dim, channel = state_vec.shape
+        feed_dict = {}
+        feed_dict[self.Fp] = Prev.reshape(-1, 1)
+        feed_dict[self.state_vec] = state_vec
+        feed_dict[self.rise_percent] = rise_percent
+        fetches = [self.final_F, self.reward]
+        portfolios, reward = session.run(fetches, feed_dict)
+        return portfolios, reward
